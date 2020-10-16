0d9c93de12dc44b4a0e8de156795b4fd3a9be4f4
dannyt <cobreti_@hotmail.com>
2020-10-16 09:17:50 -0400

    added startup of docker registry

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ kubectl apply -f ./dashboard/dashboard.yaml
 helm dependency build ./helm/loki-grafana
 helm dependency update ./helm/loki-grafana
 helm install grafana ./helm/loki-grafana --set-file loki-stack.prometheus.extraScrapeConfigs=./helm/loki-grafana/prometheus-consul-config.yaml
+cd ./docker-registry
+docker-compose up -d
+cd ..
