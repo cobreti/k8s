bb41fae4ff690c825814986127ccc0bdd2573df9
dannyt <cobreti_@hotmail.com>
2020-10-16 10:00:03 -0400

    added wait when installing grafana

--- install.ps1
+++ install.ps1
@@ -3,7 +3,7 @@ kubectl apply -f ./nginx-ingress/deploy.yaml
 kubectl apply -f ./dashboard/dashboard.yaml
 helm dependency build ./helm/loki-grafana
 helm dependency update ./helm/loki-grafana
-helm install grafana ./helm/loki-grafana --set-file loki-stack.prometheus.extraScrapeConfigs=./helm/loki-grafana/prometheus-consul-config.yaml
+helm install grafana ./helm/loki-grafana --set-file loki-stack.prometheus.extraScrapeConfigs=./helm/loki-grafana/prometheus-consul-config.yaml --wait
 cd ./docker-registry
 docker-compose up -d
 cd ..
